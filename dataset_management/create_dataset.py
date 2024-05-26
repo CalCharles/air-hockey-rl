3e04eba46dcad22511b7b6d73878fbb98ea98633
Cal Charles <calebc@cs.utexas.edu>
2024-05-26 16:01:22 -0500

    create dataset

--- dataset_management/create_dataset.py
+++ dataset_management/create_dataset.py
@@ -40,7 +40,7 @@ def load_dataset(data_dir, obs_type, environment):
             print("added trajectory ", file)
             puck_history = [(-1,0,0) for i in range(5)]
             observation, state_info = get_observation(paddle[0], paddle_vel[0], puck[0], puck_history, obs_type=obs_type)
-            observations = []
+            observations = [observation]
             next_observations = list()
             rewards = [environment.get_base_reward(state_info)]
             for pa, pav, pu in zip(paddle[1:], paddle_vel[1:], puck[1:]):
