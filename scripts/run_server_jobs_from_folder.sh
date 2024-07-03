5327ea67a56582183cdc4a23d77c901aaf08e2f9
Sarthak Dayal <sarthak@utexas.edu>
2024-07-03 15:09:50 -0500

    Make training sequential instead of parallel

--- scripts/run_server_jobs_from_folder.sh
+++ scripts/run_server_jobs_from_folder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directory containing the YAML files
-CONFIG_DIR="/home/air_hockey/air-hockey-rl/configs/baseline_configs/boox2d"
+CONFIG_DIR="/home/air_hockey/air-hockey-rl/configs/baseline_configs/box2d"
 
 # List all YAML files in the directory
 CONFIG_FILES=($CONFIG_DIR/*.yaml)
@@ -21,14 +21,21 @@ TMUX_SESSION_NAME="training_session"
 
 # Source machine details
 SOURCE_MACHINE="air_hockey@pearl-cluster.local"
-SOURCE_DIR="/home/air_hockey/air-hockey-rl/baseline_models/batch_runs"
+SOURCE_DIR="/home/air_hockey/air-hockey-rl/baseline_models/batch_runs/robosuite"
+
+# Initialize an array to store commands for each machine
+declare -A MACHINE_COMMANDS
+
+# Define valid machines for cases where machines are down/buggy
+VALID_MACHINES=(1 2 3 4 5 6 7)
 
 # Loop through the YAML files and distribute them across the machines
 for i in "${!CONFIG_FILES[@]}"; do
   CONFIG_FILE="${CONFIG_FILES[$i]}"
-  MACHINE_NUM=$(( (i % NUM_MACHINES) + 1 ))
+  MACHINE_NUM_INDEX=$(( (i % NUM_MACHINES) ))
+  MACHINE_NUM=${VALID_MACHINES[$MACHINE_NUM_INDEX]}
   MACHINE="${MACHINE_BASE_NAME}${MACHINE_NUM}.local"
-
+  
   if [ "$MACHINE_NUM" -eq 7 ]; then
     REMOTE_DIR="$MACHINE_7_REMOTE_DIR"
   else
@@ -36,13 +43,14 @@ for i in "${!CONFIG_FILES[@]}"; do
   fi
 
   REMOTE_CONFIG_FILE="$REMOTE_DIR/$(basename $CONFIG_FILE)"
-  MONITOR_SCRIPT="/tmp/monitor_and_cleanup_${i}.sh"
-
+  
   # Copy the configuration file to the remote machine
   scp "$CONFIG_FILE" "$MACHINE:$REMOTE_CONFIG_FILE"
 
   # Extract task name from YAML file
   TASK_NAME=$(yq '.air_hockey.task' "$CONFIG_FILE")
+
+  MONITOR_SCRIPT="/tmp/monitor_and_cleanup_$TASK_NAME.sh"
 
   # Create monitoring and cleanup script
   cat <<EOF > $MONITOR_SCRIPT
@@ -63,16 +71,16 @@ fi
 conda activate sarthak_rl_35
 python scripts/train.py --cfg $REMOTE_CONFIG_FILE --device cuda
 AFTER=\$(ls $REMOTE_DIR/baseline_models/$TASK_NAME)
-NEW_FOLDERS=\$(comm -13 <(echo "\$BEFORE") <(echo "\$AFTER"))
+NEW_FOLDERS=\$(comm -13 <(echo "\$BEFORE" | tr ' ' '\\n') <(echo "\$AFTER" | tr ' ' '\\n'))
 echo "Transferring folders"
 # Transfer the new folders back to the source machine
 for folder in \$NEW_FOLDERS; do
-  scp -r "$REMOTE_DIR/baseline_models/$TASK_NAME/\$folder" "$SOURCE_MACHINE:$SOURCE_DIR"
-  if [ $? -eq 0 ]; then
+  scp -r "$REMOTE_DIR/baseline_models/$TASK_NAME/\$folder" "$SOURCE_MACHINE:$SOURCE_DIR/$TASK_NAME/"
+  if [ \$? -eq 0 ]; then
     # Remove the folder if it was transferred successfully
-    rm -r "$REMOTE_DIR/baseline_models/$TASK_NAME/$folder"
+    rm -r "$REMOTE_DIR/baseline_models/$TASK_NAME/\$folder"
   else
-    echo "Error transferring $folder. It has not been removed from the machine."
+    echo "Error transferring \$folder. It has not been removed from the machine."
   fi
 done
 
@@ -84,15 +92,29 @@ rm -- "\$0"
 EOF
 
   # Copy the monitoring script to the remote machine
-  scp "$MONITOR_SCRIPT" "$MACHINE:$REMOTE_DIR/monitor_and_cleanup_${i}.sh"
+  scp "$MONITOR_SCRIPT" "$MACHINE:$REMOTE_DIR/monitor_and_cleanup_$TASK_NAME.sh"
   rm "$MONITOR_SCRIPT"
+
+  # Append the command to the list for the respective machine
+  MACHINE_COMMANDS[$MACHINE]+="bash $REMOTE_DIR/monitor_and_cleanup_$TASK_NAME.sh && "
+done
+
+# Create a tmux window on each machine and run the tasks sequentially
+for MACHINE_NUM in "${VALID_MACHINES[@]}"; do
+  MACHINE="${MACHINE_BASE_NAME}${MACHINE_NUM}.local"
+  
+  if [ "$MACHINE_NUM" -eq 7 ]; then
+    REMOTE_DIR="$MACHINE_7_REMOTE_DIR"
+  else
+    REMOTE_DIR="$DEFAULT_REMOTE_DIR"
+  fi
 
   # Construct the command to create a tmux session and window
   REMOTE_CMD="
 if ! tmux has-session -t $TMUX_SESSION_NAME 2>/dev/null; then
   tmux new-session -d -s $TMUX_SESSION_NAME
 fi
-tmux new-window -t $TMUX_SESSION_NAME -n training_${i}
+tmux new-window -t $TMUX_SESSION_NAME -n training_$MACHINE_NUM
 "
 
   # Execute the command on the remote machine to create the tmux window
@@ -100,8 +122,12 @@ tmux new-window -t $TMUX_SESSION_NAME -n training_${i}
   echo "$REMOTE_CMD"
   ssh -t "$MACHINE" "$REMOTE_CMD"
 
-  # Send the training command to the new tmux window
-  TRAIN_CMD="bash $REMOTE_DIR/monitor_and_cleanup_${i}.sh"
-  ssh -t "$MACHINE" "tmux send-keys -t $TMUX_SESSION_NAME:training_${i} \"$TRAIN_CMD\" C-m"
+  # Retrieve the commands for the current machine
+  TRAIN_CMDS=${MACHINE_COMMANDS[$MACHINE]}
+  
+  # Remove the trailing ' && '
+  TRAIN_CMDS=${TRAIN_CMDS%&& }
 
+  # Send the sequential training commands to the tmux window
+  ssh -t "$MACHINE" "tmux send-keys -t $TMUX_SESSION_NAME:training_$MACHINE_NUM \"$TRAIN_CMDS\" C-m"
 done
