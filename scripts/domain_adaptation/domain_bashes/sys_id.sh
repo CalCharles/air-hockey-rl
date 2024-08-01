a201c0eac08e72519d7c0192aa8d2863a1648b88
Cal Charles <calebc@cs.utexas.edu>
2024-08-01 13:18:50 -0500

    sys_id

--- scripts/domain_adaptation/domain_bashes/sys_id.sh
+++ scripts/domain_adaptation/domain_bashes/sys_id.sh
@@ -1,2 +1,2 @@
-python scripts/train.py --cfg configs/baseline_configs/robosuite/paddle_position_robosuite.yaml > robo_paddle_pos.yaml
-python scripts/train.py --cfg configs/baseline_configs/robosuite/puck_vel_robosuite.yaml > robo_puck_vel.yaml
+python scripts/train.py --cfg configs/baseline_configs/robosuite/paddle_position_robosuite.yaml > robo_paddle_pos.txt
+python scripts/train.py --cfg configs/baseline_configs/robosuite/puck_vel_robosuite.yaml > robo_puck_vel.txt
