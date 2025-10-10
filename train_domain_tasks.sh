f145e7a3bd3451a7576fb69b5e85252c478b226c
danielliu04 <liudaniel2004@gmail.com>
2025-10-10 16:23:05 -0500

    moved some config files and training scripts around

--- train_domain_tasks.sh
+++
@@ -1,6 +0,0 @@
-python3.12 -m scripts.train --cfg scripts/domain_adaptation/optimal_tasks/puck_goal_pos.yaml &
-python3.12 -m scripts.train --cfg scripts/domain_adaptation/optimal_tasks/puck_height.yaml &
-python3.12 -m scripts.train --cfg scripts/domain_adaptation/optimal_tasks/puck_juggle.yaml &
-python3.12 -m scripts.train --cfg scripts/domain_adaptation/optimal_tasks/puck_touch.yaml &
-python3.12 -m scripts.train --cfg scripts/domain_adaptation/optimal_tasks/puck_vel.yaml &
-python3.12 -m scripts.train --cfg scripts/domain_adaptation/optimal_tasks/strike.yaml
