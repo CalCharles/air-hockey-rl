Fix coordinate broadcasting error correctly
- Convert 2D pos to 3D by adding z=0 component
- This ensures desired_qpos has shape (3,) to match expected indexing
- Fixes IndexError: index 2 is out of bounds for axis 0 with size 2

airhockey/sims/airhockey_robosuite.py
@@ -492,8 +492,9 @@ class AirHockeyRobosuite(AirHockeySim):
             xpos = self.robosuite_env.sim.data.body_xpos[self.robosuite_env.sim.model.body_name2id(name)]
             
             pos = self.initial_block_positions[name]
-            # Only use x,y components of xpos since pos is 2D
-            desired_qpos = pos - xpos[:2]
+            # Convert 2D pos to 3D by adding z=0, then subtract 3D xpos
+            pos_3d = np.array([pos[0], pos[1], 0])
+            desired_qpos = pos_3d - xpos
             
             joint_key  = self.robosuite_env.sim.model.get_joint_qpos_addr(name + "_x")
             self.robosuite_env.sim.data.qpos[joint_key] = desired_qpos[0]